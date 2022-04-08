b35ebb5e8b2753de4ed32adc8dd811d0af4cc25b
minuxx <minux8905@gmail.com>
2022-04-09 00:06:22 +0900

    Set local module in data layer

---
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/data/local/entity/WordInfoEntity.kt
@@ -0,0 +1,25 @@
+package com.plcoding.dictionary.feature_dictionary.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.plcoding.dictionary.feature_dictionary.domain.model.License
+import com.plcoding.dictionary.feature_dictionary.domain.model.Meaning
+import com.plcoding.dictionary.feature_dictionary.domain.model.WordInfo
+
+@Entity
+data class WordInfoEntity(
+    val word: String,
+    val phonetic: String,
+    val license: License,
+    val meanings: List<Meaning>,
+    @PrimaryKey val id: Int? = null
+) {
+    fun toWordInfo(): WordInfo {
+        return WordInfo(
+            meanings = meanings,
+            word = word,
+            license = license,
+            phonetic = phonetic
+        )
+    }
+}
